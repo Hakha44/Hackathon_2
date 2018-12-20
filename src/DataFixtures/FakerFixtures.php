8961ecae5631205f05e59b25ebc2939c7e7b078c
Julien D <julien.delbez@hotmail.fr>
2018-12-20 09:37:59 +0100

    last fix fixture

--- src/DataFixtures/FakerFixtures.php
+++ src/DataFixtures/FakerFixtures.php
@@ -22,18 +22,18 @@ class FakerFixtures extends Fixture implements DependentFixtureInterface
     {
         $faker = Faker\Factory::create('fr_FR');
 
-        for ($a=0; $a < 5; $a++) {
+        for ($a = 0; $a < 5; $a++) {
             for ($i = 0; $i < 10; $i++) {
                 $event = new Event();
                 $event->setName($faker->sentence);
                 $event->setDate($faker->dateTimeBetween('-1 month', '+1 years'));
                 $manager->persist($event);
-                $event->setSubservice($this->getReference('subservice_'.$a));
+                $event->setSubservice($this->getReference('subservice_' . $a));
             }
         }
         $manager->flush();
-
     }
+
     public function getDependencies()
     {
         return [SubserviceFixtures::class];
